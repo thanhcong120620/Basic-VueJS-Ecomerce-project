424b7a2e46df3e520cb17c0a65a5d0e258daedc4
Thanh Công <nguyenthanhcong@gmail.com>
2024-07-28 22:44:07 +0700

    Test axios lần 1

---
+++ EcomerceSource/src/views/Category/AddCategory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Categories</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from '@/api/api'
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  created() {
+    api
+      .getPosts()
+      .then((response) => {
+        this.posts = response.data
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+}
+</script>
--- EcomerceSource/src/views/HomeView.vue
+++ EcomerceSource/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <div class="homePage">
+      <h1>This is home page</h1>
+    </div>
   </main>
 </template>
